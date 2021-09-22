27b735be2d3dbceb3288eb829d54afbb204388df
Dezerey Escanuelas <dezerey11@gmail.com>
2021-09-22 16:20:32 -0700

    first commit

---
+++ unknown-never.ts
@@ -0,0 +1,18 @@
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "Max";
+if (typeof userInput === "string") {
+  userName = userInput;
+}
+// userName = userInput;
+
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+  // while(true)
+}
+
+generateError("An error occured!", 500);
+// const result = generateError("An error occured!", 500);
+// console.log(result);
